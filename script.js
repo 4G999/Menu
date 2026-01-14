eea0edbde47ac2ed009bb25ea7293d1f2568ab46
4G999 <web4g999@gmail.com>
2026-01-14 17:00:08 +0800

    Refactor contact and rules popup functionality

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
+openContact.onclick = () => contactPopup.style.display = "flex";
+closeContact.onclick = () => contactPopup.style.display = "none";
+contactPopup.onclick = e => {
+    if (e.target === contactPopup) contactPopup.style.display = "none";
+};
 
-    const openBtn = document.getElementById('openContact');
-    const closeBtn = document.getElementById('closeContact');
-    const popup = document.getElementById('contactPopup');
+openRules.onclick = () => rulesPopup.style.display = "flex";
+closeRules.onclick = () => rulesPopup.style.display = "none";
+rulesPopup.onclick = e => {
+    if (e.target === rulesPopup) rulesPopup.style.display = "none";
+};
 
-    openBtn.onclick = () => popup.style.display = 'flex';
-    closeBtn.onclick = () => popup.style.display = 'none';
 
-    popup.onclick = (e) => {
-        if (e.target === popup) popup.style.display = 'none';
-    };
 
+const background = document.querySelector(".background");
+
+function createBlink() {
+  const blink = document.createElement("div");
+  blink.classList.add("blink");
+
+  // Posisi random
+  blink.style.left = Math.random() * 100 + "vw";
+
+  // Ukuran random
+  const size = Math.random() * 6 + 2;
+  blink.style.width = size + "px";
+  blink.style.height = size + "px";
+
+  // Durasi jatuh random
+  const duration = Math.random() * 8 + 5;
+  blink.style.animationDuration = `${duration}s, 1.5s`;
+
+  background.appendChild(blink);
+
+  // Hapus setelah selesai
+  setTimeout(() => {
+    blink.remove();
+  }, duration * 1000);
+}
+
+// Buat terus menerus
+setInterval(createBlink, 100);
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll(".nav-link");
+
+    links.forEach(link => {
+        link.addEventListener("click", function () {
+            links.forEach(l => l.classList.remove("active"));
+            this.classList.add("active");
+        });
+    });
+});
